4f9e63c4876c17454f3d76cd07c14a549697e77c
catitgirl <natagan888@gmail.com>
2022-12-06 23:27:37 +0400

    add simple slider

---
+++ assets/styles/script.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll(".image-container img");
+let i = 0; // current slide
+let j = 4; // total slides
+function next() {
+    document.getElementById("content" + (i + 1)).classList.remove("active");
+    i = (j + i + 1) % j;
+    document.getElementById("content" + (i + 1)).classList.add("active");
+    indicator(i + 1);
+}
+
+function prev() {
+    document.getElementById("content" + (i + 1)).classList.remove("active");
+    i = (j + i - 1) % j;
+    document.getElementById("content" + (i + 1)).classList.add("active");
+    indicator(i + 1);
+}
